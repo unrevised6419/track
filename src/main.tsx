8d165bfd26330128fbf57c5139592fdbf605c153
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2023-10-09 10:54:23 +0300

    feat: try to add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import "tailwindcss/tailwind.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import { App } from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<App />
+		<Analytics />
 	</React.StrictMode>,
 );
