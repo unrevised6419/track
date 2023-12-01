21c56c2a05068665c6aa6537bc55f9afba227e6a
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2023-12-01 03:20:02 +0200

    feat: play alarm 10m before EOD

--- src/TotalInfo.tsx
+++ src/TotalInfo.tsx
@@ -1,25 +1,36 @@
-import { useMemo } from "react";
-import { secondsToHumanFormat, cn, useLiveTotalTime } from "./utils";
+import { useEffect, useMemo } from "react";
+// @ts-expect-error - no types
+import { useSound } from "use-sound";
+import { msToHumanFormat, cn, useLiveTotalTime, storageKey } from "./utils";
 import { useAppContext } from "./AppProvider";
+import { useLocalStorage } from "@uidotdev/usehooks";
 
 export function TotalInfo() {
 	const { projects } = useAppContext();
-	const totalTime = useLiveTotalTime(projects);
-	const totalTimeHuman = useMemo(
-		() => secondsToHumanFormat(totalTime),
-		[totalTime],
+	const [playAlarm] = useSound("/call-to-attention.mp3");
+	const [alarmSoundWasPlayed, setAlarmSoundWasPlayed] = useLocalStorage(
+		storageKey("alarm-sound-was-played"),
+		false,
 	);
 
+	const totalTime = useLiveTotalTime(projects);
+	const totalTimeHuman = useMemo(() => msToHumanFormat(totalTime), [totalTime]);
+
 	const overtime = useMemo(() => {
-		const eightHoursInSeconds = 8 * 60 * 60;
-		const overtime = totalTime - eightHoursInSeconds;
+		const eightHoursInMs = 8 * 60 * 60 * 1000;
+		const overtime = totalTime - eightHoursInMs;
 		return overtime > 0 ? overtime : 0;
 	}, [totalTime]);
 
-	const overtimeHuman = useMemo(
-		() => secondsToHumanFormat(overtime),
-		[overtime],
-	);
+	const overtimeHuman = useMemo(() => msToHumanFormat(overtime), [overtime]);
+
+	const h7m50inMs = 7 * 60 * 60 * 1000 + 50 * 60 * 1000;
+	const shouldAlarm = totalTime > h7m50inMs;
+
+	useEffect(() => {
+		if (!alarmSoundWasPlayed && shouldAlarm) playAlarm();
+		setAlarmSoundWasPlayed(shouldAlarm);
+	}, [alarmSoundWasPlayed, playAlarm, setAlarmSoundWasPlayed, shouldAlarm]);
 
 	return (
 		<aside className="bg-gray-200 p-3 rounded-md font-mono text-sm grid grid-cols-2">
