265338e86b682a9c34b3b325f9698d1d7501476b
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2023-12-04 00:17:19 +0200

    refactor: remove logs migration code

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,20 +1,14 @@
 import { useLocalStorage } from "@uidotdev/usehooks";
-import {
-	Dispatch,
-	PropsWithChildren,
-	SetStateAction,
-	useCallback,
-	useMemo,
-} from "react";
-import { Interval, Log, Project } from "./types";
+import { PropsWithChildren, useCallback, useMemo } from "react";
+import { Log, Project } from "./types";
 import { groupBy, isStartedProject, storageKey } from "./utils";
 import { AppContext } from "./app-context";
 
 export function AppProvider({ children }: PropsWithChildren) {
-	const [projects, setProjects] = useProjects();
-	const [logs, setLogs] = useLocalStorage<Log[]>(
-		storageKey("logs"),
-		useMigrateOldLogs(projects),
+	const [logs, setLogs] = useLocalStorage<Log[]>(storageKey("logs"), []);
+	const [projects, setProjects] = useLocalStorage<Project[]>(
+		storageKey("projects"),
+		[],
 	);
 
 	const activeProjects = useMemo(
@@ -43,57 +37,4 @@ export function AppProvider({ children }: PropsWithChildren) {
 			{children}
 		</AppContext.Provider>
 	);
-}
-
-function useProjects() {
-	const [projects, _setProjects] = useLocalStorage<Project[]>(
-		storageKey("projects"),
-		[],
-	);
-
-	// TODO: Remove migration after a while
-	const setProjects: Dispatch<SetStateAction<Project[]>> = useCallback(
-		function (value) {
-			const voidTimes = (projects: Project[]) =>
-				projects.map((p: Project) => ({ ...p, times: undefined }));
-
-			if (typeof value !== "function") {
-				_setProjects(voidTimes(value));
-			} else {
-				_setProjects((projects) => voidTimes(value(projects)));
-			}
-		},
-		[_setProjects],
-	);
-
-	return [projects, setProjects] as const;
-}
-
-// TODO: Remove migration after a while
-type OldProject = Project & {
-	times?: Array<{
-		projectSlug: string;
-		activityName: string;
-		startedAt: number;
-		endedAt: number;
-	}>;
-};
-function useMigrateOldLogs(projects: OldProject[]) {
-	return useMemo(() => {
-		const oldLogs = projects
-			.flatMap((project) => {
-				return (project.times ?? []).map((t) => ({
-					...t,
-					projectSlug: project.slug,
-					activityName: t.activityName || project.name,
-				}));
-			})
-			.sort((t1, t2) => t2.endedAt - t1.endedAt);
-
-		return oldLogs.map((log) => ({
-			projectSlug: log.projectSlug,
-			activityName: log.activityName,
-			interval: [log.startedAt, log.endedAt] as Interval,
-		}));
-	}, [projects]);
 }
