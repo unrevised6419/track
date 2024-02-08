3880268adaf449558276766fd9ad546d68382988
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2024-02-09 01:37:45 +0200

    refactor: combine ProjectRow and ProjectActions

--- src/ProjectRow.tsx
+++ src/ProjectRow.tsx
@@ -2,12 +2,26 @@ import {
 	HiPauseCircle,
 	HiPlayCircle,
 	HiBars3BottomLeft,
+	HiArrowPath,
+	HiClipboardDocumentList,
+	HiMinusCircle,
+	HiPencil,
 } from "react-icons/hi2";
 import { Button } from "./Button";
-import { ProjectActions } from "./ProjectActions";
 import { ProjectInfo } from "./ProjectInfo";
-import { Interval, Project, ProjectAction } from "./types";
-import { cn, useDataContext } from "./utils";
+import { Interval, Project, ProjectAction, projectActions } from "./types";
+import {
+	askForActivityName,
+	cn,
+	getLegend,
+	logsTimeline,
+	useDataContext,
+	useWithClick,
+} from "./utils";
+import { useMediaQuery } from "@uidotdev/usehooks";
+import { useMemo } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
+import { ShowMoreDropdown } from "./ShowMoreDropdown";
 
 type ProjectRowProps = {
 	project: Project;
@@ -18,17 +32,133 @@ type ProjectRowProps = {
 	order: number;
 };
 
+type ProjectActionProps = {
+	action: (project: Project) => void;
+	icon: JSX.Element;
+	disabled?: boolean;
+};
+
 export function ProjectRow({
 	project,
-	projectButtons,
+	projectButtons: actions,
 	intervalMinutes,
 	timelineLength,
 	constraints,
 	order,
 }: ProjectRowProps) {
-	const { toggleActiveProject, getProjectStartedLogs } = useDataContext();
+	const isSmallDevice = useMediaQuery("(max-width : 640px)");
 
-	const isStarted = getProjectStartedLogs(project).length > 0;
+	const {
+		getProjectLogs,
+		resetProject,
+		removeProject,
+		lastActivities,
+		setLastActivities,
+		getProjectStartedLogs,
+		toggleActiveProject,
+	} = useDataContext();
+
+	const projectLogs = getProjectLogs(project);
+	const isStarted = useMemo(
+		() => getProjectStartedLogs(project).length > 0,
+		[getProjectStartedLogs, project],
+	);
+
+	const onResetProject = useWithClick(resetProject);
+	const onRemoveProject = useWithClick(removeProject);
+	const onRenameProjectActivity = useWithClick((project: Project) => {
+		const activityName = askForActivityName(lastActivities[project.slug]);
+
+		if (activityName) {
+			setLastActivities({
+				...lastActivities,
+				[project.slug]: activityName,
+			});
+		}
+	});
+
+	const onCopyProjectLog = useWithClick((project: Project) => {
+		const timeline = logsTimeline({
+			constraints,
+			logs: projectLogs,
+			intervalMinutes,
+			timelineLength,
+		});
+
+		const activities = projectLogs
+			.map((l) => l.activityName)
+			.filter((a) => a !== project.name);
+
+		const uniqueActivities = [...new Set(activities)];
+
+		const log = [
+			uniqueActivities.map((a) => `- ${a}`).join("\n"),
+			"",
+			timeline,
+			getLegend(intervalMinutes),
+		].join("\n");
+
+		void navigator.clipboard.writeText(log);
+	});
+
+	useHotkeys(
+		`${order}`,
+		() => {
+			toggleActiveProject(project);
+		},
+		[project],
+	);
+
+	const ProjectActionsMapper: Record<ProjectAction, ProjectActionProps> = {
+		copy: {
+			action: onCopyProjectLog,
+			icon: <HiClipboardDocumentList size={20} />,
+			disabled: projectLogs.length === 0,
+		},
+		remove: {
+			action: onRemoveProject,
+			icon: <HiMinusCircle size={20} />,
+		},
+		reset: {
+			action: onResetProject,
+			icon: <HiArrowPath size={20} />,
+			disabled: projectLogs.length === 0,
+		},
+		rename: {
+			action: onRenameProjectActivity,
+			icon: <HiPencil size={20} />,
+		},
+	};
+
+	const renderActions = (actions: ReadonlyArray<ProjectAction>) => {
+		return actions.map((button) => (
+			<Button
+				key={button}
+				onClick={() => {
+					ProjectActionsMapper[button].action(project);
+				}}
+				className={cn(isStarted ? "btn-error" : undefined)}
+				disabled={ProjectActionsMapper[button].disabled}
+			>
+				{ProjectActionsMapper[button].icon}
+			</Button>
+		));
+	};
+
+	let projectActionsElement;
+
+	if (isSmallDevice && actions.length > 1) {
+		projectActionsElement = (
+			<ShowMoreDropdown>
+				{/* Render all the actions, no matter what is selected */}
+				<div className="flex gap-2">{renderActions(projectActions)}</div>
+			</ShowMoreDropdown>
+		);
+	} else {
+		projectActionsElement = (
+			<div className="flex gap-3">{renderActions(actions)}</div>
+		);
+	}
 
 	return (
 		<article key={project.slug} className="flex gap-3">
@@ -57,15 +187,7 @@ export function ProjectRow({
 				</div>
 			</div>
 
-			<ProjectActions
-				project={project}
-				actions={projectButtons}
-				index={order}
-				toggleActiveProject={toggleActiveProject}
-				intervalMinutes={intervalMinutes}
-				timelineLength={timelineLength}
-				constraints={constraints}
-			/>
+			{projectActionsElement}
 		</article>
 	);
 }
